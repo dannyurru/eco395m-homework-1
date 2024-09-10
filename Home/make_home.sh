dab52e924529e21133bd4f49de35aa8c64d792c0
Danny Urrutia <daniel.urrutia@utexas.edu>
2024-09-10 17:02:39 -0500

    homies

---
+++ Home/make_home.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir Home
+cd Home
+mkdir "laundry room" "main bath" "half bath" "master bedroom" "coats closet" "home garage" "office space"
+cd "laundry room"
+touch washer.txt dryer.txt
+cd ..
+cd "main bath"
+touch doublesink.txt shower.txt toilet.txt
+cd ..
+cd "half bath"
+touch toilet.txt sink.txt
+cd ..
+cd "master bedroom"
+touch bed.txt dresser.txt closet.txt desk.txt
+cd ..
+cd "coats closet"
+touch coats.txt
+cd ..
+cd "home garage"
+touch cars.txt bikes.txt
+cd ..
+cd "office space"
